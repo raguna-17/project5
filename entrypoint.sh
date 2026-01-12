1399ae6b9e69696a4dc82e538afd7b86f024d205
らぐな <pasupoto17@gmail.com>
2026-01-12 18:28:10 +0900

    kklkl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for PostgreSQL to be ready..."
-while ! nc -z $DB_HOST 5432; do
-  echo "PostgreSQL is unavailable - sleeping"
-  sleep 1
-done
-echo "PostgreSQL is up - continuing..."
-
-# マイグレーション
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-# collectstatic
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Gunicorn or runserver
 echo "Starting server..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
