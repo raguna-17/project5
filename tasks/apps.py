47d93049725b03d45b548b50e39c603141a6346c
らぐな <pasupoto17@gmail.com>
2026-01-12 19:22:29 +0900

    kklkl

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        if not User.objects.filter(username="raguna").exists():
+            User.objects.create_superuser("raguna", "pasupoto17@gmail.com", "kaibasensei")
